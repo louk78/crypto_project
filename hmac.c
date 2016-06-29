6493caaeb9da625e6a04e4021ee6bec7ffe837ab
Jim Zhong <zhonghc@staff.zjutv.com>
2016-06-29 16:44:07 +0800

    fix error when k > 512bits

--- hmac.c
+++ hmac.c
@@ -9,18 +9,25 @@ void hmac_sha256(uint8_t *data, unsigned long length, uint8_t *key, unsigned int
     sha256_ctx_t ctx;
     int i;
 
+    if(keylen <= SHA256_BLOCK_SIZE)
+    {
+        memset(ikey, 0, SHA256_BLOCK_SIZE);
+        memset(okey, 0, SHA256_BLOCK_SIZE);
+        memcpy(ikey, key, keylen);
+        memcpy(okey, key, keylen);
+    }
+    else
+    {
+        sha256_init(&ctx);
+        sha256_update(&ctx, key, keylen);
+        sha256_finish(&ctx, ikey);
+        memcpy(okey, ikey, SHA256_BLOCK_SIZE);
+    }
+
     for(i = 0; i < SHA256_BLOCK_SIZE; i++)
     {
-        if(i < keylen)
-        {
-            ikey[i] = key[i] ^ 0x36;
-            okey[i] = key[i] ^ 0x5c;
-        }
-        else
-        {
-        ikey[i] = 0x36;
-        okey[i] = 0x5c;
-        }
+        ikey[i] = ikey[i] ^ 0x36;
+        okey[i] = okey[i] ^ 0x5c;
     }
 
     sha256_init(&ctx);
